b7509e2634a39f165416ab7c45a29974f8670edf
qaraee13566532 <davoud.qaraee@gmail.com>
2022-11-30 02:23:27 +0330

    desktop layout updated

--- lib/responsive/desktop_screen..dart
+++ lib/responsive/desktop_screen..dart
@@ -11,7 +11,10 @@ class DesktopScreen extends StatelessWidget {
       ),
       body: Container(
         margin: EdgeInsets.all(12),
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+            color: Theme.of(context).primaryColor.withOpacity(0.3),
+        ),
       ),
     );
   }
