cd8f7ef4329ef0cbb079e37c9d7ded95f4874f69
Davoud Gharaei <gharaei@takinelectronics.co>
2022-12-11 20:41:11 +0330

    some ui changes add to progect .

--- lib/views/weight_scale_ui/setting_bar.dart
+++ lib/views/weight_scale_ui/setting_bar.dart
@@ -9,10 +9,24 @@ class SettingBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         width: 45,
-        color: Colors.lightBlue,
-        child:ElevatedButton(
-         child: Container(),
-          onPressed : onPressed,
+        child:GestureDetector(
+         child: Container(
+           decoration:
+            BoxDecoration(
+               gradient: LinearGradient(
+                 begin: Alignment.topLeft,
+                 end: Alignment.topRight,
+                 colors: [
+                   Color(0xffE2EEFC),
+                   Color(0xffB7D4F7),
+                 ],
+               )
+           ),
+
+           width: double.infinity,
+//           color: Color(0xFFC5DDF9),
+         ),
+          onTap : onPressed,
         )
     );
   }
--- lib/views/weight_scale_ui/weight_scale_ui.dart
+++ lib/views/weight_scale_ui/weight_scale_ui.dart
@@ -9,119 +9,174 @@ class WeightScaleUI extends StatefulWidget {
 }
 
 class _WeightScaleUIState extends State<WeightScaleUI> {
-  bool drwaKeys=true;
+  bool drwaKeys = true;
 
+
+  final elvShaped = ElevatedButton.styleFrom(
+    backgroundColor: Color(0xffB5B5B5),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(3.0),
+
+
+    ),
+  );
+
+  List<String> items = ["کالاها","مشتریان","تراکنش","وظایف","ابزار","پس دادن","جمع کل"];//List.generate(7, (index) => index.toString());
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Column(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                      flex: 29,
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  flex: 2,
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                        height: 55,
-                                        color: Colors.green,
-                                      ),
-                                      Container(
-                                        height: 55,
-                                        color: Colors.green,
-                                      ),
-                                      Container(
-                                        height: 55,
-                                        color: Colors.green,
-                                      ),
-                                      Container(
-                                        height: 55,
-                                        color: Colors.greenAccent,
-                                      ),
-                                      Container(
-                                        height: 30,
-                                        color: Colors.white60,
-                                      ),
-                                      Container(
-                                        height: 25,
-                                        color: Colors.white60,
-                                      ),
-                                      Container(
-                                        height: 20,
-                                        color: Colors.white60,
-                                      ),
-                                      Expanded(
-                                        child: ListView.builder(
-                                          itemCount: 50,
-                                          itemBuilder: (context, index) {
-                                            return Container(
-                                              height: 25,
-                                              width: double.infinity,
-                                              color:(index%2)==0 ? Colors.white : Colors.grey
-                                             );
-                                          },
-                                        )
-                                      ),
-                                      Container(
-                                        height: 75,
-                                        color: Colors.purpleAccent,
-                                      ),
-                                    ],
+      children: [
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                  flex: 29,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 55,
+                                    color: Color(0xff366092),
                                   ),
-                                ),
-                                Expanded(
-                                    flex: 1,
-                                    child: Column(
-                                      children: [
-                                        Container(
-                                          height: 50,
-                                          color: Colors.blueAccent,
-                                        ),
-                                        Container(
-                                          height: 50,
-                                          color: Colors.orange,
-                                        ),
-                                        Expanded(
-                                          child: Container(
-                                            color: Colors.purple,
-                                          ),
-                                        ),
+                                  Container(
+                                    height: 55,
+                                    color: Color(0xff366092),
+                                  ),
+                                  Container(
+                                    height: 55,
+                                    color: Color(0xff366092),
+                                  ),
+                                  Container(
+                                    height: 55,
+                                    color: Color(0xff366092),
+                                  ),
+                                  Container(
+                                    height: 30,
+                                    decoration: BoxDecoration(
+                                        gradient: LinearGradient(
+                                      begin: Alignment.topCenter,
+                                      end: Alignment.bottomCenter,
+                                      colors: [
+                                        Color(0xffFDFDFE),
+                                        Color(0xff94A5C9),
                                       ],
                                     )),
-                              ],
+                                  ),
+                                  Container(
+                                    height: 25,
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Color(0xffDEDFE2), width: 1),
+                                      color: Color(0xffABBBD7),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 25,
+                                    decoration: BoxDecoration(
+                                        gradient: LinearGradient(
+                                      begin: Alignment.topCenter,
+                                      end: Alignment.bottomCenter,
+                                      colors: [
+                                        Color(0xffFDFDFE),
+                                        Color(0xff94A5C9),
+                                      ],
+                                    )),
+                                  ),
+                                  Expanded(
+                                      child: ListView.builder(
+                                    itemCount: 50,
+                                    itemBuilder: (context, index) {
+                                      return Container(
+                                        height: 20,
+                                        width: double.infinity,
+                                        color: (index % 2) == 0
+                                            ? Colors.white
+                                            : Color(0xfff0f0f0),
+                                      );
+                                    },
+                                  )),
+                                  Container(
+                                    height: 75,
+                                    color: Color(0xffACACAC),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                          drwaKeys==true ? Container(
-                            height: 50,
-                            color: Colors.red,
-                          ):Container(
-                            height: 0,
-                          ),
-                          Container(
-                            height: 25,
-                            color: Colors.pink,
-                          ),
-                        ],
-                      )),
-                  SettingBar(
-                    onPressed: (){
-                      setState(() {
-                        drwaKeys==true ? drwaKeys=false:drwaKeys=true;
-                      });
-                    },
-                  ),
-                ],
+                            Expanded(
+                                flex: 1,
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      height: 50,
+                                      color: Color(0xFFF2F5F7),
+                                    ),
+                                    Container(
+                                      height: 50,
+                                      color: Color(0xFFABBDE4),
+                                    ),
+                                    Expanded(
+                                      child: Container(
+                                        color: Color(0xFFADD1FF),
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                          ],
+                        ),
+                      ),
+                      drwaKeys == true
+                          ? Container(
+                              height: 55,
+                              color: Color(0xFFADD1FF),
+                              child: GridView.count(
+                                childAspectRatio: (2 / 1),
+                                // Create a grid with 2 columns. If you change the scrollDirection to
+
+                                // horizontal, this produces 2 rows.
+                                crossAxisCount: 7,
+                                // Generate 100 widgets that display their index in the List.
+                                children: List.generate(7, (index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.all(2.0),
+                                    child: ElevatedButton(
+
+                                        style: elvShaped,
+                                        child: Text(
+                                          items[index],
+                                          style: TextStyle(fontSize: 16,fontWeight: FontWeight.w900,color: Colors.black),
+                                        ),
+                                        onPressed: () {}),
+                                  );
+                                }),
+                              ),
+                            )
+                          : Container(
+                              height: 0,
+                            ),
+                      Container(
+                        height: 25,
+                        color: Color(0xFFF0F0F0),
+                      ),
+                    ],
+                  )),
+              SettingBar(
+                onPressed: () {
+                  setState(() {
+                    drwaKeys == true ? drwaKeys = false : drwaKeys = true;
+                  });
+                },
               ),
-            ),
-          ],
-        ));
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
