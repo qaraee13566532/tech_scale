7a0f95223943f52b7aebbcb6842181e26ebcb02d
qaraee13566532 <davoud.qaraee@gmail.com>
2022-12-21 02:10:02 +0330

    weight icons

--- lib/views/scale/weight_scale_info.dart
+++ lib/views/scale/weight_scale_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tech_scale/views/constant.dart';
 
@@ -8,33 +9,67 @@ class WeightScaleInfoCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 5,right: 5,left: 5),
+      padding: const EdgeInsets.only(top: 5, right: 5, left: 5),
       child: Container(
-        decoration: kWeightCardsStyle,
+        // decoration: kWeightCardsStyle,
         child: Row(
           children: [
             Expanded(
               flex: 3,
-              child: Text(
-                '  Min = 10 kg   Max = 500/1000 kg  e = d = 500g   (III)',
-                style: GoogleFonts.ptSans(
-                  fontSize: 14,
-                  color: Colors.white,
-                  decoration: TextDecoration.none,
+              child: Container(
+                decoration: kWeightCardsStyle,
+                child: Text(
+                  ' Min = 10 kg  Max = 500/1000 kg  e = d = 500g III',
+                  style: GoogleFonts.ptSans(
+                    fontSize: 16,
+                    color: Colors.white,
+                    decoration: TextDecoration.none,
+                  ),
+                  textAlign: TextAlign.left,
                 ),
-                textAlign: TextAlign.left,
               ),
             ),
             Expanded(
-              flex: 1,
-              child: Text(
-                '  Zero   Stable  Tare',
-                style: GoogleFonts.ptSans(
-                  fontSize: 14,
-                  color: Colors.white,
-                  decoration: TextDecoration.none,
+              child: Container(
+                color: kBackgroundWeightColor,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(8)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SvgPicture.asset(
+                      'assets/icons/Stable.svg',
+                      color: Colors.white,
+                    ),
+                        )),
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blueAccent,
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: SvgPicture.asset(
+                      'assets/icons/Tare.svg',
+                      color: Colors.white,
+                    ),
+                        )),
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blueAccent,
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.5),
+                          child: SvgPicture.asset(
+                      'assets/icons/Zero.svg',
+                      color: Colors.white,
+                    ),
+                        )),
+                  ],
                 ),
-                textAlign: TextAlign.center,
               ),
             ),
           ],
