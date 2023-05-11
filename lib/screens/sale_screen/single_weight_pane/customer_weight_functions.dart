0458b18360b5826e4a795cb3f1a30aef0ba15f49
qaraee13566532 <davoud.qaraee@gmail.com>
2023-05-12 00:31:16 +0330

    some arrangement applied.

--- lib/screens/sale_screen/single_weight_pane/customer_weight_functions.dart
+++ lib/screens/sale_screen/single_weight_pane/customer_weight_functions.dart
@@ -3,42 +3,39 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tech_scale/screens/sale_screen/dual_weight_pane/floating_customer_card.dart';
 import 'package:tech_scale/utils/constant.dart';
 
-
 class CustomerWeightFunctions extends StatelessWidget {
   final List<int> badgeValues;
   final List<VoidCallback> weightCustomerTasks;
-  const CustomerWeightFunctions({Key? key, required this.badgeValues,required this.weightCustomerTasks})
+
+  const CustomerWeightFunctions(
+      {Key? key, required this.badgeValues, required this.weightCustomerTasks})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: List.generate(
-          6,
-          (index) => FloatingCustomerCard(
-            widgetIcon:
-                index == 0
-                    ? Text(
-                        'صفر',
-                        style: kWeightCardTitleStyle.copyWith(
-                            color: Colors.indigo, fontWeight: FontWeight.bold),
-                      )
-                    : index == 1
-                        ? Text('پارسنگ',
-                            style: kWeightCardTitleStyle.copyWith(
-                                color: Colors.indigo,
-                                fontWeight: FontWeight.bold))
-                        : const Icon(
-                            FontAwesomeIcons.userLarge,
-                            size: 30,
-                            color: kBackgroundWeightColor,
-                          ),
-            badgeValue: badgeValues[index],
-            hasBadge: (index > 1),
-            onTap: weightCustomerTasks[index] ,
-          ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(
+        6,
+        (index) => FloatingCustomerCard(
+          widgetIcon: index == 0
+              ? Text(
+                  'صفر',
+                  style: kWeightCardTitleStyle.copyWith(
+                      color: Colors.indigo, fontWeight: FontWeight.bold),
+                )
+              : index == 1
+                  ? Text('پارسنگ',
+                      style: kWeightCardTitleStyle.copyWith(
+                          color: Colors.indigo, fontWeight: FontWeight.bold))
+                  : const Icon(
+                      FontAwesomeIcons.userLarge,
+                      size: 30,
+                      color: kBackgroundWeightColor,
+                    ),
+          badgeValue: badgeValues[index],
+          hasBadge: (index > 1),
+          onTap: weightCustomerTasks[index],
         ),
       ),
     );
